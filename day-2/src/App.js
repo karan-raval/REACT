df4c241ae130e6923198d2b726b16ee82e3359f3
karan-raval <karanraval424@gmail.com>
2024-08-22 08:38:52 +0530

    Day-2 Webpage In REACT

---
+++ day-2/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Footer from './Components/Footer';
+import Navbar from './Components/Navbar'
+import Section1 from './Components/Section-1';
+function App() {
+  
+  return (
+<>
+<Navbar></Navbar>
+<Section1/>
+<Footer/>
+</>
+  );
+}
+
+export default App;
