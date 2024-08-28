d41f8d78799041a32b6c2d09d2f616a4151a5691
karan-raval <karanraval424@gmail.com>
2024-08-28 18:39:03 +0530

    Day-14 Web Page With React Bootstrap

---
+++ day-14(Projecct)/src/Components/Section4.jsx
@@ -0,0 +1,433 @@
+import React from 'react'
+import Card from 'react-bootstrap/Card';
+import ProgressBar from 'react-bootstrap/ProgressBar';
+import CountUp from 'react-countup';
+
+
+const Section4 = () => {
+  return (
+    <>
+    <section className="ftco-section ftco-causes ftco-no-pb" id="causes-section">
+<div className="container">
+<div className="row justify-content-center pb-5">
+<div className="col-md-12 heading-section text-center ftco-animate">
+<span className="subheading">Donate Charity</span>
+<h2 className="mb-5">Causes Needs Our Help</h2>
+<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia</p>
+</div>
+</div>
+<div className="row">
+<div className="col-md-6 col-lg-3">
+<div className="cause-entry ftco-animate">
+<a href="#" className="img s4img1" ></a>
+<div className="text p-3">
+<h3><a href="cause.html">Health care, Food</a></h3>
+<div className=" mb-4"> 
+<ProgressBar label={`40%`} variant="warning" now={40} />
+</div> 
+<p className="donate"><span className="price">$80,000</span> <span className="target-goal">Target</span> / <span className="price">$55,000</span> <span className="target-goal">Goal</span></p>
+<p>A small river named Duden flows by their place and supplies it with the necessary.</p>
+<p><a href="#" className="btn btn-primary">Donate Now!</a></p>
+</div>
+</div>
+</div>
+<div className="col-md-6 col-lg-3">
+<div className="cause-entry ftco-animate">
+<a  className="img s4img2" ></a>
+<div className="text p-3">
+<h3><a href="cause.html">Restored the Old City Church</a></h3>
+<div className=" mb-4"> 
+<ProgressBar label={`50%`} variant="warning" now={50} />
+</div> 
+<p className="donate"><span className="price">$80,000</span> <span className="target-goal">Target</span> / <span className="price">$55,000</span> <span className="target-goal">Goal</span></p>
+<p>A small river named Duden flows by their place and supplies it with the necessary.</p>
+<p><a href="#" className="btn btn-primary">Donate Now!</a></p>
+</div>
+</div>
+</div>
+<div className="col-md-6 col-lg-3">
+<div className="cause-entry ftco-animate">
+<a href="#" className="img s4img3" ></a>
+<div className="text p-3">
+<h3><a href="cause.html">Save Childern For Hunger</a></h3>
+<div className=" mb-4"> 
+<ProgressBar label={`80%`} variant="warning" now={80} />
+</div> 
+<p className="donate"><span className="price">$80,000</span> <span className="target-goal">Target</span> / <span className="price">$55,000</span> <span className="target-goal">Goal</span></p>
+<p>A small river named Duden flows by their place and supplies it with the necessary.</p>
+<p><a href="#" className="btn btn-primary">Donate Now!</a></p>
+</div>
+</div>
+</div>
+<div className="col-md-6 col-lg-3">
+<div className="cause-entry ftco-animate">
+<a href="#" className="img s4img4" ></a>
+<div className="text p-3">
+<h3><a href="cause.html">Save Childern For Hunger</a></h3>
+<div className=" mb-4"> 
+<ProgressBar label={`80%`} variant="warning" now={80} />
+</div> 
+<p className="donate"><span className="price">$80,000</span> <span className="target-goal">Target</span> / <span className="price">$55,000</span> <span className="target-goal">Goal</span></p>
+<p>A small river named Duden flows by their place and supplies it with the necessary.</p>
+<p><a href="#" className="btn btn-primary">Donate Now!</a></p>
+</div>
+</div>
+</div>
+</div>
+<div className="row mt-4 justify-content-end">
+<div className="col-lg-10">
+<div className="donated-intro bg-darken p-4 p-md-5">
+<h2>Donation so far <span>$</span><strong className="number"><CountUp end={9891000} duration={5} className="number">0</CountUp></strong></h2>
+<p style={{color:'#999999'}}>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics</p>
+<p><a href="#" className="btn btn-black px-4 py-3">Donate now!</a></p>
+</div>
+</div>
+</div>
+</div>
+</section>
+<section className="ftco-daily-verse img s4img5" >
+<div className="overlay"></div>
+<div className="container">
+<div className="row justify-content-center">
+<div className="col-md-10">
+<div className="row justify-content-center align-items-center">
+<div className="col-md-2 ftco-animate">
+<div className="icon">
+<span className="flaticon-church"></span>
+</div>
+</div>
+<div className="col-md-10 daily-verse pl-md-5 ftco-animate">
+<span className="subheading">Jesus Christ Said:</span>
+<h3>"16 For God so loved the world, that he gave his only begotten Son, that whosoever believeth in him should not perish, but have everlasting life."</h3>
+<h4 className="h5 mt-4 font-weight-bold"><span>Bible:</span> John 3:16 KJV</h4>
+</div>
+</div>
+</div>
+</div>
+</div>
+</section>
+<section className="ftco-section bg-light" id="blog-section">
+<div className="container">
+<div className="row justify-content-center mb-5 pb-5">
+<div className="col-md-7 heading-section text-center ftco-animate">
+<span className="subheading">Blog</span>
+<h2 className="mb-4">Read the Latest Blog</h2>
+<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia</p>
+</div>
+</div>
+<div className="row d-flex">
+<div className="col-md-4 d-flex ftco-animate">
+<div className="blog-entry justify-content-end">
+<a href="single.html" className="block-20 s4img6" >
+</a>
+<div className="text float-right d-block">
+<div className="d-flex align-items-center pt-2 mb-4 topp">
+<div className="one mr-2">
+<span className="day">04</span>
+</div>
+<div className="two">
+<span className="yr">2019</span>
+<span className="mos">June</span>
+</div>
+</div>
+<h3 className="heading"><a href="single.html">Why Lead Generation is Key for Business Growth</a></h3>
+<p>A small river named Duden flows by their place and supplies it with the necessary regelialia.</p>
+<div className="d-flex align-items-center mt-4 meta">
+<p className="mb-0"><a href="#" className="btn btn-primary">Read More <span className="ion-ios-arrow-round-forward"></span></a></p>
+<p className="ml-auto mb-0">
+<a href="#" className="mr-2">Admin</a>
+<a href="#" className="meta-chat"><span className="icon-chat"></span> 3</a>
+</p>
+</div>
+</div>
+</div>
+</div>
+<div className="col-md-4 d-flex ftco-animate">
+<div className="blog-entry justify-content-end">
+<a href="single.html" className="block-20 s4img7" >
+</a>
+<div className="text float-right d-block">
+<div className="d-flex align-items-center pt-2 mb-4 topp">
+<div className="one mr-2">
+<span className="day">04</span>
+</div>
+<div className="two">
+<span className="yr">2019</span>
+<span className="mos">June</span>
+</div>
+</div>
+<h3 className="heading"><a href="single.html">Why Lead Generation is Key for Business Growth</a></h3>
+<p>A small river named Duden flows by their place and supplies it with the necessary regelialia.</p>
+<div className="d-flex align-items-center mt-4 meta">
+<p className="mb-0"><a href="#" className="btn btn-primary">Read More <span className="ion-ios-arrow-round-forward"></span></a></p>
+<p className="ml-auto mb-0">
+<a href="#" className="mr-2">Admin</a>
+<a href="#" className="meta-chat"><span className="icon-chat"></span> 3</a>
+</p>
+</div>
+</div>
+</div>
+</div>
+<div className="col-md-4 d-flex ftco-animate">
+<div className="blog-entry">
+<a href="single.html" className="block-20 s4img8" >
+</a>
+<div className="text float-right d-block">
+<div className="d-flex align-items-center pt-2 mb-4 topp">
+<div className="one mr-2">
+<span className="day">04</span>
+</div>
+<div className="two">
+<span className="yr">2019</span>
+<span className="mos">June</span>
+</div>
+</div>
+<h3 className="heading"><a href="single.html">Why Lead Generation is Key for Business Growth</a></h3>
+<p>A small river named Duden flows by their place and supplies it with the necessary regelialia.</p>
+<div className="d-flex align-items-center mt-4 meta">
+<p className="mb-0"><a href="#" className="btn btn-primary">Read More <span className="ion-ios-arrow-round-forward"></span></a></p>
+<p className="ml-auto mb-0">
+<a href="#" className="mr-2">Admin</a>
+<a href="#" className="meta-chat"><span className="icon-chat"></span> 3</a>
+</p>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</section>
+<section className="ftco-section ftco-no-pb" id="pastor-section">
+<div className="container">
+<div className="row justify-content-center pb-5">
+<div className="col-md-6 heading-section text-center ftco-animate">
+<span className="subheading">Pastors</span>
+<h2 className="mb-4">Church Pastor</h2>
+<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia</p>
+</div>
+</div>
+<div className="row">
+<Card className='col-md-6 col-lg-3 ftco-animate'>
+<div className="staff">
+<div className="img-wrap d-flex align-items-stretch">
+      <Card.Img variant="top" width={100} src="https://preview.colorlib.com/theme/christian/images/staff-1.jpg.webp" className='img align-self-stretch' />
+      </div>
+      <div className="text d-flex align-items-center pt-3 text-center">
+      <Card.Body>
+        <Card.Title>Lloyd Wilson</Card.Title>
+        <span className="position mb-4">Lead Pastor</span>
+        <Card.Text>
+        <div className="faded">
+<ul className="ftco-social text-center">
+<li className="ftco-animate"><a href="#"><span className="icon-twitter"></span></a></li>
+<li className="ftco-animate"><a href="#"><span className="icon-facebook"></span></a></li>
+<li className="ftco-animate"><a href="#"><span className="icon-google-plus"></span></a></li>
+<li className="ftco-animate"><a href="#"><span className="icon-instagram"></span></a></li>
+</ul>
+</div>
+        </Card.Text>
+      </Card.Body>
+      </div>
+      </div>
+    </Card>
+    <Card className='col-md-6 col-lg-3 ftco-animate'>
+<div className="staff">
+<div className="img-wrap d-flex align-items-stretch">
+      <Card.Img variant="top" width={100} src="https://preview.colorlib.com/theme/christian/images/staff-3.jpg.webp" className='img align-self-stretch' />
+      </div>
+      <div className="text d-flex align-items-center pt-3 text-center">
+      <Card.Body>
+        <Card.Title>Rachel Parker</Card.Title>
+        <span className="position mb-4">Lead Pastor</span>
+        <Card.Text>
+        <div className="faded">
+<ul className="ftco-social text-center">
+<li className="ftco-animate"><a href="#"><span className="icon-twitter"></span></a></li>
+<li className="ftco-animate"><a href="#"><span className="icon-facebook"></span></a></li>
+<li className="ftco-animate"><a href="#"><span className="icon-google-plus"></span></a></li>
+<li className="ftco-animate"><a href="#"><span className="icon-instagram"></span></a></li>
+</ul>
+</div>
+        </Card.Text>
+      </Card.Body>
+      </div>
+      </div>
+    </Card>
+    <Card className='col-md-6 col-lg-3 ftco-animate'>
+<div className="staff">
+<div className="img-wrap d-flex align-items-stretch">
+      <Card.Img variant="top" width={100} src="https://preview.colorlib.com/theme/christian/images/staff-4.jpg.webp" className='img align-self-stretch' />
+      </div>
+      <div className="text d-flex align-items-center pt-3 text-center">
+      <Card.Body>
+        <Card.Title>Ian Smith</Card.Title>
+        <span className="position mb-4">Lead Pastor</span>
+        <Card.Text>
+        <div className="faded">
+<ul className="ftco-social text-center">
+<li className="ftco-animate"><a href="#"><span className="icon-twitter"></span></a></li>
+<li className="ftco-animate"><a href="#"><span className="icon-facebook"></span></a></li>
+<li className="ftco-animate"><a href="#"><span className="icon-google-plus"></span></a></li>
+<li className="ftco-animate"><a href="#"><span className="icon-instagram"></span></a></li>
+</ul>
+</div>
+        </Card.Text>
+      </Card.Body>
+      </div>
+      </div>
+    </Card>
+    <Card className='col-md-6 col-lg-3 ftco-animate'>
+<div className="staff">
+<div className="img-wrap d-flex align-items-stretch">
+      <Card.Img variant="top" width={100} src="https://preview.colorlib.com/theme/christian/images/staff-2.jpg.webp" className='img align-self-stretch' />
+      </div>
+      <div className="text d-flex align-items-center pt-3 text-center">
+      <Card.Body>
+        <Card.Title>Alicia Henderson</Card.Title>
+        <span className="position mb-4">Lead Pastor</span>
+        <Card.Text>
+        <div className="faded">
+<ul className="ftco-social text-center">
+<li className="ftco-animate"><a href="#"><span className="icon-twitter"></span></a></li>
+<li className="ftco-animate"><a href="#"><span className="icon-facebook"></span></a></li>
+<li className="ftco-animate"><a href="#"><span className="icon-google-plus"></span></a></li>
+<li className="ftco-animate"><a href="#"><span className="icon-instagram"></span></a></li>
+</ul>
+</div>
+        </Card.Text>
+      </Card.Body>
+      </div>
+      </div>
+    </Card>
+
+
+
+
+</div>
+</div>
+</section>
+<section className="ftco-section contact-section ftco-no-pb" id="contact-section">
+<div className="container">
+<div className="row justify-content-center mb-5 pb-3">
+<div className="col-md-7 heading-section text-center ftco-animate">
+<span className="subheading">Contact</span>
+<h2 className="mb-4">Contact Me</h2>
+<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia</p>
+</div>
+</div>
+<div className="row block-9">
+<div className="col-md-7 order-md-last d-flex">
+<form action="#" className="bg-light p-4 p-md-5 contact-form">
+<div className="form-group">
+<input type="text" className="form-control" placeholder="Your Name"/>
+</div>
+<div className="form-group">
+<input type="text" className="form-control" placeholder="Your Email"/>
+</div>
+<div className="form-group">
+<input type="text" className="form-control" placeholder="Subject"/>
+</div>
+<div className="form-group">
+<textarea name id cols="30" rows="7" className="form-control" placeholder="Message"></textarea>
+</div>
+<div className="form-group">
+<input type="submit" value="Send Message" className="btn btn-primary py-3 px-5"/>
+</div>
+</form>
+</div>
+<div className="col-md-5 d-flex">
+<div className="row d-flex contact-info mb-5">
+<div className="col-md-12 ftco-animate">
+<div className="box p-2 px-3 bg-light d-flex">
+<div className="icon mr-3">
+<span className="icon-map-signs"></span>
+</div>
+<div>
+<h3 className="mb-3">Address</h3>
+<p>198 West 21th Street, Suite 721 New York NY 10016</p>
+</div>
+</div>
+</div>
+<div className="col-md-12 ftco-animate">
+<div className="box p-2 px-3 bg-light d-flex">
+<div className="icon mr-3">
+<span className="icon-phone2"></span>
+</div>
+<div>
+<h3 className="mb-3">Contact Number</h3>
+<p><a href="tel://1234567920">+ 1235 2355 98</a></p>
+</div>
+</div>
+</div>
+<div className="col-md-12 ftco-animate">
+<div className="box p-2 px-3 bg-light d-flex">
+<div className="icon mr-3">
+<span className="icon-paper-plane"></span>
+</div>
+<div>
+<h3 className="mb-3">Email Address</h3>
+<p><a href="/cdn-cgi/l/email-protection#3e575058517e47514b4c4d574a5b105d5153"><span className="__cf_email__" data-cfemail="b8d1d6ded7f8c1d7cdcacbd1ccdd96dbd7d5">admin@123gmail.com</span></a></p>
+</div>
+</div>
+</div>
+<div className="col-md-12 ftco-animate">
+<div className="box p-2 px-3 bg-light d-flex">
+<div className="icon mr-3">
+<span className="icon-globe"></span>
+</div>
+<div>
+<h3 className="mb-3">Website</h3>
+<p><a href="#">yoursite.com</a></p>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</section>
+<section className="ftco-gallery ftco-section ftco-no-pb mb-4">
+<div className="container-fluid px-4">
+<div className="row justify-content-center mb-5 pb-3">
+<div className="col-md-7 heading-section ftco-animate text-center">
+<h3 className="subheading">Gallery</h3>
+<h2 className="mb-1">Christian Church Photo Gallery</h2>
+</div>
+</div>
+<div className="row">
+<div className="col-md-3 ftco-animate">
+<a  className="gallery image-popup img d-flex align-items-center  s4img13" >
+<div className="icon mb-4 d-flex align-items-center justify-content-center">
+<span className="icon-instagram"></span>
+</div>
+</a>
+</div>
+<div className="col-md-3 ftco-animate">
+<a href="images/gallery-2.jpg" className="gallery image-popup img d-flex align-items-center s4img14" >
+<div className="icon mb-4 d-flex align-items-center justify-content-center">
+<span className="icon-instagram"></span>
+</div>
+</a>
+</div>
+<div className="col-md-3 ftco-animate">
+<a href="images/gallery-3.jpg" className="gallery image-popup img d-flex align-items-center s4img15" >
+<div className="icon mb-4 d-flex align-items-center justify-content-center">
+<span className="icon-instagram"></span>
+</div>
+</a>
+</div>
+<div className="col-md-3 ftco-animate">
+<a href="images/gallery-4.jpg" className="gallery image-popup img d-flex align-items-center s4img16" >
+<div className="icon mb-4 d-flex align-items-center justify-content-center">
+<span className="icon-instagram"></span>
+</div>
+</a>
+</div>
+</div>
+</div>
+</section>
+    </>
+  )
+}
+
+export default Section4
