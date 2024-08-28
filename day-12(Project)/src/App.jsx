23741ec95a791f998095179de7d26dc6cd8b0170
karan-raval <karanraval424@gmail.com>
2024-08-28 18:33:40 +0530

    Day-12 Form with validation

---
+++ day-12(Project)/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Signup1 from './Components/Signup1'
+import './Components/Login.css' 
+
+
+function App() {
+
+  return (
+    <>
+    <Signup1/>
+    </>
+  )
+}
+
+export default App
