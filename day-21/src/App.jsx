475b00b3bc195045305ed0a6fac98c5861f78598
karan-raval <karanraval424@gmail.com>
2024-08-30 09:41:41 +0530

    Day-21 Explore Material-Ui

---
+++ day-21/src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+import Buttons from './Components/Buttons'
+import Icons from './Components/Icons'
+import Typographye from './Components/Typographye'
+
+function App() {
+
+  return (
+    <>
+<Typographye/>
+<br /><br /><br /><br />
+<Buttons/>
+<br /><br /><br /><br />
+<Icons/>
+    </>
+  )
+}
+
+export default App
---
+++ day-21/src/Components/Icons.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import AcUnitIcon from '@mui/icons-material/AcUnit';
+const Icons = () => {
+  return (
+    <>
+        <AcUnitIcon color='error'  fontSize='small'/>
+        <AcUnitIcon color='error'  fontSize='small'/>
+        <AcUnitIcon color='secondary' />
+        <AcUnitIcon color='primary' />
+        <AcUnitIcon color='action' fontSize='large' />
+        <AcUnitIcon color='disabled'  fontSize='large'/>
+    </>
+  )
+}
+
+export default Icons
