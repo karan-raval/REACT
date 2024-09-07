583616e69bed7a3e3e3fea96f6b171ede9bc8a0a
karan-raval <karanraval424@gmail.com>
2024-09-07 10:16:14 +0530

    Day-24 Login Signup With Redux

--- Day-24/src/Commponents/Login.jsx
+++ Day-24/src/Commponents/Login.jsx
@@ -3,19 +3,33 @@ import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import '../App.css'
 const Login = () => {
+  const dispatch = useDispatch()
+  const value = useSelector((s)=>s)
+  console.log(value)
+  const [state,setState] = useState({
+      email : "",
+      password : ""
+  })
+  const handleChange = (e)=>{
+      setState({...state,[e.target.name] : e.target.value})
+  }
+  const handleSubmit = (e)=>{
+       e.preventDefault()
+      fetchapi(dispatch,state.email)
+  }
 
   return (
     <>
       <div className="center">
         <h1>Login</h1>
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className="txt_field">
-            <input type="email" required />
+            <input type="email" required   onChange={handleChange} />
             <span></span>
             <label>Email</label>
           </div>
           <div className="txt_field">
-            <input type="password" required />
+            <input type="password" required   onChange={handleChange} />
             <span></span>
             <label>Password</label>
           </div>
--- Day-24/src/Commponents/Signup.jsx
+++ Day-24/src/Commponents/Signup.jsx
@@ -1,72 +1,93 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import '../App.css'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "../App.css";
 const Signup = () => {
+  const [state, setState] = useState({
+    email: "",
+    password: "",
+  });
+  const handleChange = (e) => {
+    setState({ ...state, [e.target.name]: e.target.value });
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    fetch(`https://mock-server-app2-dll0.onrender.com/user`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(state),
+    })
+      .then((res) => res.json())
+      .then((Res) => console.log(Res))
+      .catch((err) => console.log(err));
+  };
   return (
     <>
-    <div className="container">
-          <div className="title">Signup</div>
-          <div className="content">
-            <form>
-              <div className="user-details">
-                <div className="input-box">
-                  <span className="details">Full Name</span>
-                  <input type="text" placeholder="Enter your name"  />
-                </div>
-                <div className="input-box">
-                  <span className="details">Username<sup>*</sup></span>
-                  <input
-                    type="text"
-                    placeholder="Enter your username"
-                    required
-                  />
-                </div>
-                <div className="input-box">
-                  <span className="details">Email<sup>*</sup></span>
-                  <input
-                    type="text"
-                    placeholder="Enter your email"
-                    required
-                  />
-                </div>
-                <div className="input-box">
-                  <span className="details">Phone Number</span>
-                  <input type="text" placeholder="Enter your number"  />
-                </div>
-                <div className="input-box">
-                  <span className="details">Password <sup>*</sup></span>
-                  <input
-                    type="text"
-                    placeholder="Enter your password"
-                  
-                    required
-                  />
-                </div>
-                <div className="input-box">
-                  <span className="details">Confirm Password</span>
-                  <input
-                    type="text"
-                    placeholder="Confirm your password"
-                  />
-                </div>
+      <div className="container">
+        <div className="title">Signup</div>
+        <div className="content">
+          <form onSubmit={handleSubmit}>
+            <div className="user-details">
+              <div className="input-box">
+                <span className="details">Full Name<sup>*</sup></span>
+                <input
+                  type="text"
+                  placeholder="Enter your name"
+                  onChange={handleChange}
+                />
               </div>
-              
-              <div className="button">
-                <input type="button"  value="Submit" />
-              </div>
-              <div className="signup">
-                <span className="signup">
-                  Already have an account ?
-                  <label for="check" >
-                  <Link className='link' to={'/'}>Login</Link>
-                  </label>
+              <div className="input-box">
+                <span className="details">
+                  Username<sup>*</sup>
                 </span>
+                <input type="text" onChange={handleChange} placeholder="Enter your username" required />
               </div>
-            </form>
-          </div>
+              <div className="input-box">
+                <span className="details">
+                  Email<sup>*</sup>
+                </span>
+                <input
+                  type="text"
+                  placeholder="Enter your email"
+                  required
+                  onChange={handleChange}
+                />
+              </div>
+              <div className="input-box">
+                <span className="details">Phone Number<sup>*</sup></span>
+                <input type="text" onChange={handleChange} placeholder="Enter your number" />
+              </div>
+              <div className="input-box">
+                <span className="details">
+                  Password <sup>*</sup>
+                </span>
+                <input
+                  type="text"
+                  placeholder="Enter your password"
+                  onChange={handleChange}
+                  required
+                />
+              </div>
+            </div>
+            <div className="button">
+              <input type="button" value="Submit" />
+            </div>
+            <div className="signup">
+              <span className="signup">
+                Already have an account ?
+                <label for="check">
+                  <Link className="link" to={"/"}>
+                    Login
+                  </Link>
+                </label>
+              </span>
+            </div>
+          </form>
         </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Signup
+export default Signup;
