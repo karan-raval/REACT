4d8204a94d3088f1d7bde4b2cb6163af5a94c18c
karan-raval <karanraval424@gmail.com>
2024-09-07 10:28:56 +0530

    Day-25 Product-Page And Single Product-Page with Redux

---
+++ Day-25/src/Components/Productpage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Productpage = () => {
+  return (
+    <>
+    </>
+  )
+}
+
+export default Productpage
---
+++ Day-25/src/Route/Mainroute.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Mainroute = () => {
+  return (
+    <>
+    
+    </>  )
+}
+
+export default Mainroute
