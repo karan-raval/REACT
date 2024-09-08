bcf087031d3f23e755c9e25204c2028d84c40342
karan-raval <karanraval424@gmail.com>
2024-09-08 17:29:22 +0530

    Day-25 Product-Page And Single Product-Page with Redux

--- Day-25/src/App.jsx
+++ Day-25/src/App.jsx
@@ -1,19 +1,11 @@
-import { useState } from 'react'
 import './App.css'
-import Apiproduct from './Components/Apiproduct.jsx'
-import Navbar from './Components/Navbar.jsx'
-import Footer from './Components/Footer.jsx'
-import Login from './Pages/Login.jsx'
-
+import Mainroute from './Route/Mainroute.jsx'
 
 function App() {
 
   return (
 	<>
-  {/* <Navbar/> */}
-  {/* <Apiproduct/> */}
-  {/* <Footer/> */}
-  <Login/>
+<Mainroute/>
 		</>
   )
 }
--- Day-25/src/Route/Mainroute.jsx
+++ Day-25/src/Route/Mainroute.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Productpage from '../Pages/Productpage'
+import Singleproduct from '../Pages/Singleproduct'
 
 const Mainroute = () => {
   return (
     <>
-      
-    
+    <Routes>
+      <Route path='/' element={<Productpage/>}></Route>  
+      <Route path='/product/:id' element={<Singleproduct/>}></Route>  
+    </Routes>    
     </>
   )
 }
