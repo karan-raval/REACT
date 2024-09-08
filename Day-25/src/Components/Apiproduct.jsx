6180521b8be1ca2462da8320bd15ea1009cbd7fa
karan-raval <karanraval424@gmail.com>
2024-09-08 20:01:33 +0530

    Day-25 Product-Page And Single Product-Page with Redux

--- Day-25/src/Components/Apiproduct.jsx
+++ Day-25/src/Components/Apiproduct.jsx
@@ -1,28 +1,29 @@
 import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 function Apiproduct() {
-  const [data,setdata]=useState([])
+  const [data, setdata] = useState([]);
 
-  useEffect(()=>{
+  useEffect(() => {
     fetch(`https://mock-server-app-4tp9.onrender.com/product`)
-	.then(res=>res.json())
-	.then((res)=>{
-		setdata(res)
-	})
-	.catch((err)=>{
-		console.log(err); 
-	})
-  },[])
+      .then((res) => res.json())
+      .then((res) => {
+        setdata(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
 
   return (
     <div>
       <div className="untree_co-section product-section before-footer-section">
         <div className="container">
           <div className="row">
-            {
-            data.map((el) => {
+            {data.map((el) => {
               return (
-                <div className="col-12 col-md-4 col-lg-3 mb-5 my">
+                <div key={el.id} className="col-12 col-md-4 col-lg-3 mb-5 my">
+                  <Link to={`/product/${el.id}`}>
                   <a className="product-item">
                     <img
                       src={el.imageURL}
@@ -32,17 +33,19 @@ function Apiproduct() {
                     <strong className="product-price">${el.price}</strong>
 
                     <span className="icon-cross">
-                      <img src="https://themewagon.github.io/furni/images/cross.svg" className="img-fluid" />
+                      <img
+                        src="https://themewagon.github.io/furni/images/cross.svg"
+                        className="img-fluid"
+                      />
                     </span>
-                  </a>
+                  </a></Link>
                 </div>
-              )
-            })
-            }
+              );
+            })}
           </div>
         </div>
       </div>
-     </div>
+    </div>
   );
 }
 
--- Day-25/src/Pages/Singleproduct.jsx
+++ Day-25/src/Pages/Singleproduct.jsx
@@ -1,9 +1,134 @@
-import React from 'react'
-
+import React, { useEffect, useState } from 'react'
+import Navbar from '../Components/Navbar'
+import Footer from '../Components/Footer'
+import { useParams } from 'react-router-dom'
 const Singleproduct = () => {
+
+  const obj = useParams()
+
+  const [data, setdata] = useState([]);
+
+  useEffect(() => {
+    fetch(`https://mock-server-app-4tp9.onrender.com/product/${obj.id}`)
+      .then((res) => res.json())
+      .then((res) => {
+        setdata(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
   return (
     <>
-    
+    <Navbar/>
+    <div className="untree_co-section before-footer-section">
+            <div className="container">
+              <div className="row mb-5">
+                <form className="col-md-12" method="post">
+                  <div className="site-blocks-table">
+                    <table className="table">
+                      <thead>
+                        <tr>
+                          <th className="product-thumbnail">Image</th>
+                          <th className="product-name">Product</th>
+                          <th className="product-price">Price</th>
+                          <th className="product-quantity">Quantity</th>
+                          <th className="product-total">Total</th>
+                          <th className="product-remove">Remove</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        {<tr>
+                          <td className="product-thumbnail">
+                            <img src={data.imageURL} alt={data.imageURL} className="img-fluid"/>
+                          </td>
+                          <td className="product-name">
+                            <h2 className="h5 text-black">{data.product}</h2>
+                          </td>
+                          <td>${data.price}</td>
+                          <td>
+                            <div className="input-group mb-3 d-flex align-items-center quantity-container" style={{maxwidth:120}}>
+                              <div className="input-group-prepend">
+                                <button className="btn btn-outline-black decrease" type="button">-</button>
+                              </div>
+                              <input type="text" className="form-control text-center quantity-amount" value="1" placeholder="" aria-label="Example text with button addon" aria-describedby="button-addon1"/>
+                              <div className="input-group-append">
+                                <button className="btn btn-outline-black increase" type="button">+</button>
+                              </div>
+                            </div>
+        
+                          </td>
+                          <td>${data.price}</td>
+                          <td><a href="#" className="btn btn-black btn-sm">X</a></td>
+                        </tr>}
+        
+                       
+                      </tbody>
+                    </table>
+                  </div>
+                </form>
+              </div>
+        
+              <div className="row">
+                <div className="col-md-6">
+                  <div className="row mb-5">
+                    <div className="col-md-6 mb-3 mb-md-0">
+                      <button className="btn btn-black btn-sm btn-block">Update Cart</button>
+                    </div>
+                    <div className="col-md-6">
+                      <button className="btn btn-outline-black btn-sm btn-block">Continue Shopping</button>
+                    </div>
+                  </div>
+                  <div className="row">
+                    <div className="col-md-12">
+                      <label className="text-black h4" for="coupon">Coupon</label>
+                      <p>Enter your coupon code if you have one.</p>
+                    </div>
+                    <div className="col-md-8 mb-3 mb-md-0">
+                      <input type="text" className="form-control py-3" id="coupon" placeholder="Coupon Code"/>
+                    </div>
+                    <div className="col-md-4">
+                      <button className="btn btn-black">Apply Coupon</button>
+                    </div>
+                  </div>
+                </div>
+                <div className="col-md-6 pl-5">
+                  <div className="row justify-content-end">
+                    <div className="col-md-7">
+                      <div className="row">
+                        <div className="col-md-12 text-right border-bottom mb-5">
+                          <h3 className="text-black h4 text-uppercase">Cart Totals</h3>
+                        </div>
+                      </div>
+                      <div className="row mb-3">
+                        <div className="col-md-6">
+                          <span className="text-black">Subtotal</span>
+                        </div>
+                        <div className="col-md-6 text-right">
+                          <strong className="text-black">$230.00</strong>
+                        </div>
+                      </div>
+                      <div className="row mb-5">
+                        <div className="col-md-6">
+                          <span className="text-black">Total</span>
+                        </div>
+                        <div className="col-md-6 text-right">
+                          <strong className="text-black">$230.00</strong>
+                        </div>
+                      </div>
+        
+                      <div className="row">
+                        <div className="col-md-12">
+                          <button className="btn btn-black btn-lg py-3 btn-block">Proceed To Checkout</button>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+    <Footer/>
     </>)
 }
 
