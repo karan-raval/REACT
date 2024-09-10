edcd62b77fc74db0ab86cc9b086f69be7ed7866e
karan-raval <karanraval424@gmail.com>
2024-09-10 19:32:25 +0530

    Day-20 Admin panel Project

--- Day-20(Project)/src/Route/MainRoutes.jsx
+++ Day-20(Project)/src/Route/MainRoutes.jsx
@@ -3,13 +3,14 @@ import { Route, Routes } from 'react-router-dom'
 import Product from '../Pages/Product'
 import Singleproduct from '../Pages/Singleproduct'
 import PageNotFound from '../Pages/PageNotFound'
-
+import Home from '../Pages/Home'
 
 const MainRoutes = () => {
   return (
     <>
     <Routes>
-      <Route path="/" element={<Product/>}></Route>
+      <Route path="/" element={<Home/>}></Route>
+      <Route path="/product" element={<Product/>}></Route>
       <Route path="/product/:id" element={<Singleproduct/>}></Route>  
       <Route path="*" element={<PageNotFound/>}></Route>  
     </Routes>   
