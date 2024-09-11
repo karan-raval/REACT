7108a8282e1039abcebc3404dd198f88786db890
karan-raval <karanraval424@gmail.com>
2024-09-11 17:39:09 +0530

    Day-27 axios

--- Day-27/src/App.jsx
+++ Day-27/src/App.jsx
@@ -1,10 +1,30 @@
+import { useEffect, useState } from 'react'
 import './App.css'
+import axios from 'axios'
 
 function App() {
 
+  const [state,setstate]=useState([])
+
+
+  useEffect(()=>{
+    axios.get(`https://mock-server-app2-dll0.onrender.com/product`)
+    .then((res)=>{
+      console.log(res);
+      console.log(res.data);
+      setstate(res.data);
+    })
+    .catch((err)=>{
+      console.log(err);
+      
+    })
+  },[])
   return (
     <>
-     
+    {state.map((el)=>(
+      <h1>{el.brand}</h1>
+    ))
+    }
     </>
   )
 }
