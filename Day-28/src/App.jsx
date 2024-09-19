f969639caab2fe5326cf885f75ce29150b529e58
karan-raval <karanraval424@gmail.com>
2024-09-19 21:51:26 +0530

    Day-27 Login Signup With Firebase

--- Day-28/src/App.jsx
+++ Day-28/src/App.jsx
@@ -1,10 +1,11 @@
-import './App.css'
+// import './App.css'
+import Login from './Components/Login'
 
 function App() {
 
   return (
     <>
-      
+      <Login></Login>
     </>
   )
 }
--- Day-28/src/Components/Login.jsx
+++ Day-28/src/Components/Login.jsx
@@ -1,9 +1,38 @@
 import React from 'react'
-
+import '../assets/Login.css'
 const Login = () => {
   return (
     <>
-    
+       <div class="wrapper">
+         <div class="title">
+            Login Form
+         </div>
+         <form action="#">
+            <div class="field">
+               <input type="text" required/>
+               <label>Email Address</label>
+            </div>
+            <div class="field">
+               <input type="password" required/>
+               <label>Password</label>
+            </div>
+            <div class="content">
+               <div class="checkbox">
+                  <input type="checkbox" id="remember-me"/>
+                  <label >Remember me</label>
+               </div>
+               <div class="pass-link">
+                  <a href="#">Forgot password?</a>
+               </div>
+            </div>
+            <div class="field">
+               <input type="submit" value="Login"/>
+            </div>
+            <div class="signup-link">
+               Not a member? <a href="#">Signup now</a>
+            </div>
+         </form>
+      </div>
     </>
   )
 }
