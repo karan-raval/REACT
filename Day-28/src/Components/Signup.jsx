0ae6f45d143e717f5bf1b88872a8dd18ba9fe178
karan-raval <karanraval424@gmail.com>
2024-09-22 14:31:27 +0530

    Day-28 Login Signup With Firebase

--- Day-28/src/Components/Signup.jsx
+++ Day-28/src/Components/Signup.jsx
@@ -2,6 +2,58 @@ import React from "react";
 import "../assets/Signup.css";
 import { Link } from "react-router-dom";
 const Signup = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (state.username == "") {
+      Swal.fire({
+        icon: "error",
+        title: "Oops...",
+        text: "Fill The Username Input Field!",
+      });
+    } else if (state.email == "") {
+      Swal.fire({
+        icon: "error",
+        title: "Oops...",
+        text: "Fill The Email Input Field!",
+      });
+    } else if (state.password == "") {
+      Swal.fire({
+        icon: "error",
+        title: "Oops...",
+        text: "Fill The Password Input Field!",
+      });
+    } else if (state.fullname == "") {
+      Swal.fire({
+        icon: "error",
+        title: "Oops...",
+        text: "Fill The Full Name Input Field!",
+      });
+    } else {
+      Swal.fire({
+        icon: "success",
+        title: "Congratsss...",
+        text: "SignUp Succesfully...",
+      });
+      navigate("/login");
+
+      fetch(`https://mock-server-app2-dll0.onrender.com/user`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(state),
+      })
+        .then((res) => res.json())
+        .then((res) => console.log(res))
+        .catch((err) => console.log(err));
+    }
+    setState({
+      fullname: "",
+      username: "",
+      email: "",
+      password: "",
+    });
+  };
   return (
     <>
       <div className="login-page">
