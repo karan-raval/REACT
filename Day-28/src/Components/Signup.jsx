6b3117de6a1ebb2c8bda065ebc85ca630fd93c7b
karan-raval <karanraval424@gmail.com>
2024-09-23 12:54:13 +0530

    Day-28 Login Signup With Firebase

--- Day-28/src/Components/Signup.jsx
+++ Day-28/src/Components/Signup.jsx
@@ -2,40 +2,39 @@ import React, { useEffect, useState } from "react";
 import "../assets/Signup.css";
 import { Link, Navigate, useNavigate } from "react-router-dom";
 import { db } from "../FirebaseFolder/firebase";
-import { addDoc, collection, getDocs } from 'firebase/firestore';
-import Swal from 'sweetalert2'
+import { addDoc, collection, getDocs } from "firebase/firestore";
+import Swal from "sweetalert2";
 const Signup = () => {
-  const [d,setD] = useState([])
+  const [d, setD] = useState([]);
 
-  useEffect(()=>{
-    async function getData(){
-        let data = await getDocs(UserCollection)
-        let val = data.docs.map((el)=>{
-            return {id : el.id , ...el.data()}
-        })
-        setD(val)
+  useEffect(() => {
+    async function getData() {
+      let data = await getDocs(UserCollection);
+      let val = data.docs.map((el) => {
+        return { id: el.id, ...el.data() };
+      });
+      setD(val);
     }
-    getData()
-},[])
+    getData();
+  }, []);
 
-
-  const navigate =useNavigate()
+  const navigate = useNavigate();
   const [state, setState] = useState({
     fullname: "",
     username: "",
     email: "",
     password: "",
   });
-  const UserCollection = collection(db,"users")
+  const UserCollection = collection(db, "users");
   const handleChange = (e) => {
     setState({ ...state, [e.target.name]: e.target.value });
   };
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    let a = d.filter((el)=>{
-      return el.email == state.email
-    })
+    let a = d.filter((el) => {
+      return el.email == state.email;
+    });
     if (state.username == "") {
       Swal.fire({
         icon: "error",
@@ -61,19 +60,19 @@ const Signup = () => {
         text: "Fill The Full Name Input Field!",
       });
     } else {
-      if(a.length > 0){
-        alert("User alerady registered")
-      }else{
+      if (a.length > 0) {
+        alert("User alerady registered");
+      } else {
+        let a = await addDoc(UserCollection, state);
 
-      let a  = await addDoc(UserCollection,state)
-
-      Swal.fire({
-        icon: "success",
-        title: "Congratsss...",
-        text: "SignUp Succesfully...",
-      });
-      navigate("/");
-    }}
+        Swal.fire({
+          icon: "success",
+          title: "Congratsss...",
+          text: "SignUp Succesfully...",
+        });
+        navigate("/");
+      }
+    }
     setState({
       fullname: "",
       username: "",
@@ -86,10 +85,34 @@ const Signup = () => {
       <div className="login-page">
         <div className="form">
           <form className="register-form" onSubmit={handleSubmit}>
-            <input type="text" name="fullname" value={state.fullname} placeholder="Full Name"   onChange={handleChange} />
-            <input type="text" name="username" value={state.username} placeholder="User Name"    onChange={handleChange}/>
-            <input type="text" name="email" value={state.email} placeholder="email Address"   onChange={handleChange} />
-            <input type="password" name="password" value={state.password} placeholder="password"   onChange={handleChange} />
+            <input
+              type="text"
+              name="fullname"
+              value={state.fullname}
+              placeholder="Full Name"
+              onChange={handleChange}
+            />
+            <input
+              type="text"
+              name="username"
+              value={state.username}
+              placeholder="User Name"
+              onChange={handleChange}
+            />
+            <input
+              type="text"
+              name="email"
+              value={state.email}
+              placeholder="email Address"
+              onChange={handleChange}
+            />
+            <input
+              type="password"
+              name="password"
+              value={state.password}
+              placeholder="password"
+              onChange={handleChange}
+            />
             <button type="submit">create</button>
             <p className="message">
               Already registered? <Link to={"/"}>Login Now</Link>
