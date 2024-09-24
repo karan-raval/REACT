f173b5463fd51da0ccaf31e5911157ca6a5a706a
karan-raval <karanraval424@gmail.com>
2024-09-24 20:00:22 +0530

    Day-29 Admin panel with Firebase

---
+++ Day-29/src/Routes/Mainroutes.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from '../pages/Home'
+
+const Mainroutes = () => {
+  return (
+    <>
+    <Routes>
+        <Route path='/' element={<Home/>} ></Route>
+    </Routes>
+    </>
+  )
+}
+
+export default Mainroutes
