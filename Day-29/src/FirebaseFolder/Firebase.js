3f8d01d93f9cf075610b7c74d06fcc106eeaaf4b
karan-raval <karanraval424@gmail.com>
2024-09-24 12:56:15 +0530

    Day-29 Admin panel with Firebase

---
+++ Day-29/src/FirebaseFolder/Firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAG2rKI6YbHBaJql32AQ4OM3mwSljeLVMU",
+  authDomain: "adminpanel-6d39c.firebaseapp.com",
+  projectId: "adminpanel-6d39c",
+  storageBucket: "adminpanel-6d39c.appspot.com",
+  messagingSenderId: "724758596721",
+  appId: "1:724758596721:web:abd1656692cd8708c9dfd0"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+
