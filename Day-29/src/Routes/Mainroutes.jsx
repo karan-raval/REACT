504464784224f41710b7cabe956246ff52809e62
karan-raval <karanraval424@gmail.com>
2024-09-29 18:11:18 +0530

    Day-29 Admin panel with Firebase

--- Day-29/src/Routes/Mainroutes.jsx
+++ Day-29/src/Routes/Mainroutes.jsx
@@ -4,8 +4,6 @@ import Home from '../pages/Home'
 import Signup from '../pages/Signup'
 import Login from '../pages/Login'
 import PageNotFound from '../pages/PageNotFound'
-import AddProduct from '../pages/AddProduct'
-import ProductPage from '../pages/ProductPage'
 
 const Mainroutes = () => {
   return (
@@ -14,8 +12,6 @@ const Mainroutes = () => {
         <Route path='/' element={<Home/>} ></Route>
         <Route path='/signup' element={<Signup/>} ></Route>
         <Route path='/login' element={<Login/>} ></Route>
-        <Route path='/addproduct' element={<AddProduct/>} ></Route>
-        <Route path='/product' element={<ProductPage/>} ></Route>
         <Route path='*' element={<PageNotFound/>} ></Route>
     </Routes>
     </>
