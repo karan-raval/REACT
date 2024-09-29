ae4f59bba5e1d8c6ce4727b4e8d8a8b3ed8ba039
karan-raval <karanraval424@gmail.com>
2024-09-29 11:37:43 +0530

    Day-29 Admin panel with Firebase

--- Day-29/src/FirebaseFolder/Firebase.js
+++ Day-29/src/FirebaseFolder/Firebase.js
@@ -1,19 +1,32 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import {getFirestore} from "firebase/firestore"
+import {getAuth, GoogleAuthProvider, signInWithPopup, signOut} from "firebase/auth"
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAG2rKI6YbHBaJql32AQ4OM3mwSljeLVMU",
-  authDomain: "adminpanel-6d39c.firebaseapp.com",
-  projectId: "adminpanel-6d39c",
-  storageBucket: "adminpanel-6d39c.appspot.com",
-  messagingSenderId: "724758596721",
-  appId: "1:724758596721:web:abd1656692cd8708c9dfd0"
+  apiKey: "AIzaSyC-Lz3kpsyH64c0WMbmmHgwHrmuGmbXb_w",
+  authDomain: "admin-f26b2.firebaseapp.com",
+  projectId: "admin-f26b2",
+  storageBucket: "admin-f26b2.appspot.com",
+  messagingSenderId: "1042947080466",
+  appId: "1:1042947080466:web:83f026396759e03f8ab3cb"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
+export const db = getFirestore(app)
+export const auth = getAuth()
 
+
+export const googleAuth = ()=>{
+   let provider = new GoogleAuthProvider()
+   return signInWithPopup(auth,provider)
+}
+
+export const GoogleLogout = async ()=>{
+    return await signOut(auth)
+}
