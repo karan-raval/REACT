dad34a7c485c63011f6ef6f1cf82cf52d251c65c
karan-raval <karanraval424@gmail.com>
2024-10-04 08:58:21 +0530

    product task

--- Project_3/src/Routes/Mainroutes.jsx
+++ Project_3/src/Routes/Mainroutes.jsx
@@ -1,17 +1,17 @@
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
-import Home from '../Pages/Home'
-import ProductPage from '../Pages/ProductPage'
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "../Pages/Home";
+import ProductPage from "../Pages/ProductPage";
 
 const Mainroutes = () => {
   return (
     <>
-    <Routes>
-      <Route path='/' element={<Home/>}></Route>
-      <Route path='/product' element={<ProductPage/>}></Route>
-    </Routes>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/product" element={<ProductPage />}></Route>
+      </Routes>
     </>
-  )
-}
+  );
+};
 
-export default Mainroutes
+export default Mainroutes;
