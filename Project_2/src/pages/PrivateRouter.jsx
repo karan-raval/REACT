db10152dd6bd8973d508c5badf57f9d5088fb135
karan-raval <karanraval424@gmail.com>
2024-10-06 14:12:41 +0530

    Admin-dashbord (Project) with Firebase

--- Project_2/src/pages/PrivateRouter.jsx
+++ Project_2/src/pages/PrivateRouter.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
+import { Navigate } from 'react-router-dom';
 
 const PrivateRouter = () => {
+  const { isLoading, isError, data, ISLOGIN } = useSelector((s) => s.loginreducer);
+  console.log(ISLOGIN)
+
   return (
-    <>
-    
-    </>
+    ISLOGIN == true ? children : <Navigate to={'/login'} />
   )
 }
 
