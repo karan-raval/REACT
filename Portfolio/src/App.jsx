f83115abe672e55010f3f0fb35d444fb016d982c
karan-raval <karanraval424@gmail.com>
2024-10-07 20:44:55 +0530

    Portfolio

--- Portfolio/src/App.jsx
+++ Portfolio/src/App.jsx
@@ -1,33 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
+import './assets/app.css'
+import Sidebar from './components/sidebar/Sidebar';
+import Home from './components/home/Home';
+import About from './components/about/About';
+import Services from './components/services/Services';
+import Resume from './components/resume/Resume';
+import Portfolio from './components/portfolio/Portfolio';
+import Testimonials from './components/testimonials/Testimonials';
+import Blog from './components/blog/Blog';
+import Contact from './components/contact/Contact';
+import useLocalStorage from 'use-local-storage'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+       <div className="app" data-theme={theme}>
+            <Sidebar theme={theme} switchTheme={switchTheme} />
+            <main className='main'>
+                <Home />
+                <About />
+                <Services />
+                <Resume />
+                <Portfolio />
+                <Testimonials />
+                <Blog />
+                <Contact theme={theme} />
+            </main>
+        </div>
     </>
   )
 }
