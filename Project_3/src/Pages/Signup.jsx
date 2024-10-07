c150cf37f79b58486b0ff9641e728389a65c94a7
karan-raval <karanraval424@gmail.com>
2024-10-07 20:19:09 +0530

    Portfolio

--- Project_3/src/Pages/Signup.jsx
+++ Project_3/src/Pages/Signup.jsx
@@ -72,81 +72,78 @@ const Signup = () => {
   };
   return (
     <>
-    <ToastContainer />
+      <ToastContainer />
       <section className="vh-100 gradient-custom">
         <div className="container py-5 h-90">
           <div className="row d-flex justify-content-center align-items-center h-90">
             <div className="col-12 col-md-8 col-lg-6 col-xl-5">
-              <div className="card bg-dark text-white br" >
+              <div className="card bg-dark text-white br">
                 <div className="card-body p-5 text-center">
-                  <div className="mb-md-5 mt-md-4 pb-5">
-                    <h2 className="fw-bold mb-2 text-uppercase">Login</h2>
-                    <p className="text-white-50 mb-5">
-                      Please enter your login and password!
-                    </p>
+                  <form onSubmit={handleSubmit}>
+                    <div className="mb-md-5 mt-md-4 pb-5">
+                      <h2 className="fw-bold mb-2 text-uppercase">Login</h2>
+                      <p className="text-white-50 mb-5">
+                        Please enter your login and password!
+                      </p>
 
-                    <div
-                      data-mdb-input-init
-                      className="form-outline form-white mb-4"
-                    >
-                      <input
-                        type="email"
-                        id="typeEmailX"
-                        className="form-control form-control-lg"
-                      />
-                      <label className="form-label" for="typeEmailX">
-                        Email
-                      </label>
+                      <div
+                        data-mdb-input-init
+                        className="form-outline form-white mb-4"
+                      >
+                        <input
+                          type="email"
+                          id="typeEmailX"
+                          className="form-control form-control-lg"
+                        />
+                        <label className="form-label" for="typeEmailX">
+                          Email
+                        </label>
+                      </div>
+
+                      <div
+                        data-mdb-input-init
+                        className="form-outline form-white mb-4"
+                      >
+                        <input
+                          type="password"
+                          id="typePasswordX"
+                          className="form-control form-control-lg"
+                        />
+                        <label className="form-label" for="typePasswordX">
+                          Password
+                        </label>
+                      </div>
+
+                      <p className="small mb-5 pb-lg-2">
+                        <a className="text-white-50">Forgot password?</a>
+                      </p>
+
+                      <button
+                        data-mdb-button-init
+                        data-mdb-ripple-init
+                        className="btn btn-outline-light btn-lg px-5"
+                        type="submit"
+                      >
+                        Login
+                      </button>
+
+                      <div className="d-flex justify-content-center text-center mt-4 pt-1">
+                        <a className="text-white">
+                          <i className="fab fa-facebook-f fa-lg"></i>
+                        </a>
+                        <a className="text-white">
+                          <i className="fab fa-twitter fa-lg mx-4 px-2"></i>
+                        </a>
+                        <a className="text-white">
+                          <i className="fab fa-google fa-lg"></i>
+                        </a>
+                      </div>
                     </div>
-
-                    <div
-                      data-mdb-input-init
-                      className="form-outline form-white mb-4"
-                    >
-                      <input
-                        type="password"
-                        id="typePasswordX"
-                        className="form-control form-control-lg"
-                      />
-                      <label className="form-label" for="typePasswordX">
-                        Password
-                      </label>
-                    </div>
-
-                    <p className="small mb-5 pb-lg-2">
-                      <a className="text-white-50"  >
-                        Forgot password?
-                      </a>
-                    </p>
-
-                    <button
-                      data-mdb-button-init
-                      data-mdb-ripple-init
-                      className="btn btn-outline-light btn-lg px-5"
-                      type="submit"
-                    >
-                      Login
-                    </button>
-
-                    <div className="d-flex justify-content-center text-center mt-4 pt-1">
-                      <a   className="text-white">
-                        <i className="fab fa-facebook-f fa-lg"></i>
-                      </a>
-                      <a   className="text-white">
-                        <i className="fab fa-twitter fa-lg mx-4 px-2"></i>
-                      </a>
-                      <a   className="text-white">
-                        <i className="fab fa-google fa-lg"></i>
-                      </a>
-                    </div>
-                  </div>
-
+                  </form>
                   <div>
                     <p className="mb-1">
                       Don't have an account?{" "}
-                      <a   className="text-danger-50 fw-bold">
-                        Sign Up
-                      </a>
+                      <a className="text-danger-50 fw-bold">Sign Up</a>
                     </p>
                   </div>
                 </div>
