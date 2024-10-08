c02a4da7a1dc3762c9dc86c5a51bfbc2be233f9f
karan-raval <karanraval424@gmail.com>
2024-10-08 22:27:29 +0530

    Portfolio

--- Portfolio/src/components/home/HeaderSocials.jsx
+++ Portfolio/src/components/home/HeaderSocials.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import { FaGithub, FaFacebookF, FaLinkedinIn, FaTwitter, FaDev } from 'react-icons/fa';
-import { FaInstagram } from "react-icons/fa";
+import { FaGithub, FaLinkedinIn } from 'react-icons/fa';
 const HeaderSocials = () => {
     return (
         <div className='home__socials'>
@@ -8,25 +7,9 @@ const HeaderSocials = () => {
                 <FaGithub />
             </a>
 
-            <a href='https://hashnode.com/@gregsithole' className='home__social-link' target='_blank' rel='noreferrer'>
-            <FaInstagram />
-            </a>
-
-            {/* <a href='https://dev.to/gregsithole' className='home__social-link' target='_blank' rel='noreferrer'>
-                <FaDev />
-            </a> */}
-
             <a href='https://www.linkedin.com/in/karan-raval-8b5b0b203/' className='home__social-link' target='_blank' rel='noreferrer'>
                 <FaLinkedinIn />
             </a>
-
-            {/* <a href='https://wwww.facebook.com/Greg.Sithole' className='home__social-link' target='_blank' rel='noreferrer'>
-                <FaFacebookF />
-            </a> */}
-
-            {/* <a href='https://www.twitter.com/GregSithole' className='home__social-link' target='__blank' rel='noreferrer'>
-                <FaTwitter />
-            </a> */}
         </div>
     );
 };
