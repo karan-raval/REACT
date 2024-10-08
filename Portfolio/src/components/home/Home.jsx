f45838992c91b85cc3c3acc6448e085e4f116bd4
karan-raval <karanraval424@gmail.com>
2024-10-08 22:40:49 +0530

    Portfolio

--- Portfolio/src/components/home/Home.jsx
+++ Portfolio/src/components/home/Home.jsx
@@ -1,29 +1,23 @@
-import React from 'react';
-import './Home.css';
-import Me from '../../assets/2.gif';
-import HeaderSocials from './HeaderSocials';
-import ScrollDown from './ScrollDown';
-import Shapes from './Shapes';
+import React from "react";
+import "./Home.css";
+import Me from "../../assets/2.gif";
+import HeaderSocials from "./HeaderSocials";
+import ScrollDown from "./ScrollDown";
+import Shapes from "./Shapes";
 
 const Home = () => {
-    return (
+  return (
+    <section className="home container" id="home">
+      <div className="intro">
+        <img src={Me} className="home__img" width="320" />
+        <h1 className="home__name">Karan Raval</h1>
+        <span className="home__education">I'm into Full-stack Devloper</span>
+        <HeaderSocials />
+        <ScrollDown />
+      </div>
+      <Shapes />
+    </section>
+  );
+};
 
-        <section className="home container" id='home'>
-            <div className="intro">
-                <img src={Me} className='home__img' width='320' />
-                <h1 className="home__name">Karan Raval</h1>
-                <span className="home__education">I'm into Full-stack Devloper</span>
-
-                <HeaderSocials />
-
-                {/* <a href="#contact" className="btn"> Contact Me</a> */}
-
-                <ScrollDown />
-            </div>
-
-            <Shapes />
-        </section>
-    )
-}
-
-export default Home
+export default Home;
