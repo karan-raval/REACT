32c6c175d41dd76069fc1211801ab04b554995dd
karan-raval <karanraval424@gmail.com>
2024-10-11 10:55:47 +0530

    product task

--- Project_3/src/Routes/Mainroutes.jsx
+++ Project_3/src/Routes/Mainroutes.jsx
@@ -4,6 +4,7 @@ import Home from "../Pages/Home";
 import ProductPage from "../Pages/ProductPage";
 import Login from "../Pages/Login";
 import Signup from "../Pages/Signup";
+import Cart from "../Pages/Cart";
 
 const Mainroutes = () => {
   return (
@@ -13,6 +14,7 @@ const Mainroutes = () => {
         <Route path="/product" element={<ProductPage />}></Route>
         <Route path="/login" element={<Login />}></Route>
         <Route path="/signup" element={<Signup />}></Route>
+        <Route path="/cart" element={<Cart />}></Route>
       </Routes>
     </>
   );
