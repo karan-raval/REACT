45d9f8b2cb1d845e606a4a29e92906faa3db0174
karan-raval <karanraval424@gmail.com>
2024-10-11 11:02:54 +0530

    product task

--- Project_3/src/Pages/Cart.jsx
+++ Project_3/src/Pages/Cart.jsx
@@ -33,37 +33,27 @@ const Cart = () => {
                     <th className="product-thumbnail">Image</th>
                     <th className="product-name">Product</th>
                     <th className="product-price">Price</th>
-                    <th className="product-quantity">Quantity</th>
-                    <th className="product-total">Total</th>
+                    <th className="product-quantity">Category</th>
+                    {/* <th className="product-total">Total</th> */}
                     <th className="product-remove">Remove</th>
                   </tr>
                 </thead>
                 <tbody>
                  
-
-                  <tr>
+                {state.product.map((el) => {
+                    return (
+                  <tr key={el.id}>
                     <td className="product-thumbnail">
-                      <img src="images/cloth_2.jpg" alt="Image" className="img-fluid"/>
+                      <img src={el.imageURL} alt="Image" className="img-fluid"/>
                     </td>
                     <td className="product-name">
-                      <h2 className="h5 text-black">Polo Shirt</h2>
+                      <h2 className="h5 text-black">{el.product}</h2>
                     </td>
-                    <td>$49.00</td>
-                    <td>
-                      <div className="input-group mb-3" style={{maxWidth: 120}}>
-                        <div className="input-group-prepend">
-                          <button className="btn btn-outline-primary js-btn-minus" type="button">-</button>
-                        </div>
-                        <input type="text" className="form-control text-center" value="1" placeholder="" aria-label="Example text with button addon" aria-describedby="button-addon1"/>
-                        <div className="input-group-append">
-                          <button className="btn btn-outline-primary js-btn-plus" type="button">+</button>
-                        </div>
-                      </div>
-
-                    </td>
-                    <td>$49.00</td>
+                    <td>{el.category}</td>
+                    <td>${el.price}</td>
                     <td><a href="#" className="btn btn-primary btn-sm">X</a></td>
                   </tr>
+                   )})}
                 </tbody>
               </table>
             </div>
