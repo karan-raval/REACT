b19604c4d20a6b77cefd68283247eac4351e7314
karan-raval <karanraval424@gmail.com>
2024-10-14 04:37:53 +0530

    MY Portfolio

--- Portfolio/src/components/home/Home.jsx
+++ Portfolio/src/components/home/Home.jsx
@@ -1,27 +1,46 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./Home.css";
 import Me from "../../assets/2.gif";
 import HeaderSocials from "./HeaderSocials";
 import ScrollDown from "./ScrollDown";
 import Shapes from "./Shapes";
-// import $ from "jquery";
+import Typed from "typed.js";
 
 const Home = () => {
+  useEffect(() => {
+    // Initializing Typed.js when the component is mounted
+    const options = {
+      strings: [
+        "Full-Stack Web Development.", 
+        "Backend Development", 
+        "Frontend  Development", 
+      ],
+      typeSpeed: 50,
+      backSpeed: 30,
+      loop: true,
+    };
     
+    const typed = new Typed(".typing-text", options);
+
+    return () => {
+      typed.destroy();
+    };
+  }, []); 
+
   return (
     <>
-    <div id="particles-js">
-    <section className="home container"   id="home">
-      <div className="intro">
-        <img src={Me} className="home__img" width="320" />
-        <h1 className="home__name">Karan Raval</h1>
-        <span className="home__education">I'm into Full-stack Devloper</span>
-        <HeaderSocials />
-        <ScrollDown />
-      </div>
-      <Shapes />
-    </section>
-    </div>
+        <section className="home container" id="home">
+          <div className="intro">
+            <img src={Me} className="home__img" width="320" alt="Karan Raval" />
+            <h1 className="home__name">Karan Raval</h1>
+            <span className="home__education">
+              I'm into <span className="typing-text"></span>
+            </span>
+            <HeaderSocials />
+            <ScrollDown />
+          </div>
+          <Shapes />
+        </section>
     </>
   );
 };
