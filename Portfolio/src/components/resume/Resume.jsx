3da98fcdddf63c4ca55b6935ace694ca79a9f7af
karan-raval <karanraval424@gmail.com>
2024-10-15 15:59:36 +0530

    MY Portfolio

--- Portfolio/src/components/resume/Resume.jsx
+++ Portfolio/src/components/resume/Resume.jsx
@@ -2,38 +2,116 @@ import React, { useState } from "react";
 import "./Resume.css";
 import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
 import Button from "./Button";
+import { TiHtml5 } from "react-icons/ti";
+import { span } from "framer-motion/client";
+import { FaCss3Alt } from "react-icons/fa";
+import { FaJs } from "react-icons/fa";
+import { TbJson } from "react-icons/tb";
+import { DiJqueryLogo } from "react-icons/di";
+import { FaBootstrap } from "react-icons/fa";
+import { SiVite } from "react-icons/si";
+import { SiReactrouter } from "react-icons/si";
+import { SiNpm } from "react-icons/si";
+import { FaReact } from "react-icons/fa";
+import { RiTailwindCssFill } from "react-icons/ri";
+import { SiMui } from "react-icons/si";
+import { VscVscode } from "react-icons/vsc";
+import { SiPostman } from "react-icons/si";
+import { FaChrome } from "react-icons/fa";
+import { SiSublimetext } from "react-icons/si";
+import { SiWebstorm } from "react-icons/si";
+import { FiCodesandbox } from "react-icons/fi";
+import { FaGithub } from "react-icons/fa";
+import { FaGitAlt } from "react-icons/fa";
+import { SiNetlify } from "react-icons/si";
+import { SiVercel } from "react-icons/si";
+import { SiRender } from "react-icons/si";
+import { FaWindows } from "react-icons/fa";
+import { SiFirebase } from "react-icons/si";
+import { FaNode } from "react-icons/fa6";
+import { SiMongodb } from "react-icons/si";
+import { SiExpress } from "react-icons/si";
+import { SiPrettier } from "react-icons/si";
+import { SiRedux } from "react-icons/si";
+import { SiPassport } from "react-icons/si";
+import { SiAuth0 } from "react-icons/si";
+import { BsRegex } from "react-icons/bs";
+import { FaCloudflare } from "react-icons/fa6";
+import { SiMongoose } from "react-icons/si";
+
+const IconComponent = ({ icon: Icon, name, color, hoverColor }) => {
+  return (
+    <div className="icon-wrapper">
+      <Icon className="icon" />
+      <span className="icon-name">{name}</span>{" "}
+      {/* The name is always in the DOM but hidden */}
+    </div>
+  );
+};
 
 const WorkExperience = [
   {
     id: 1,
-    title: "Software Engineer",
-    company: "FrontEnd",
-    yearsActive: "CSS",
+    title: "Programming Languages , Frameworks & Libraries",
+    company: "Front-End",
+    yearsActive: "html",
     information: [
-      "Write modern, performant, maintainable code for a diverse array of internal projects",
-      "Communicate with multi-disciplinary teams of engineers, team leads, and QA Engineers on a daily basis",
+      <TiHtml5 />,
+      <FaCss3Alt />,
+      <FaJs />,
+      <DiJqueryLogo />,
+      <FaBootstrap />,
+      <RiTailwindCssFill />,
+      <IconComponent
+        icon={FaReact}
+        name="React"
+        color="white"
+        hoverColor="#21a1f1"
+      />,
+      <IconComponent icon={SiVite} name="vite" color="white" hoverColor="#21a1f1" />,
+      <SiReactrouter />,
+      <SiNpm />,
+      <SiMui />,
+      <TbJson />,
+      <SiRedux />,
     ],
-    img:"data:image/svg+xml;base64,PHN2ZyBmaWxsPSJ3aGl0ZSIgcm9sZT0iaW1nIiB2aWV3Qm94PSIwIDAgMjQgMjQiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+PHRpdGxlPkNTUzM8L3RpdGxlPjxwYXRoIGQ9Ik0xLjUgMGgyMWwtMS45MSAyMS41NjNMMTEuOTc3IDI0bC04LjU2NS0yLjQzOEwxLjUgMHptMTcuMDkgNC40MTNMNS40MSA0LjQxbC4yMTMgMi42MjIgMTAuMTI1LjAwMi0uMjU1IDIuNzE2aC02LjY0bC4yNCAyLjU3M2g2LjE4MmwtLjM2NiAzLjUyMy0yLjkxLjgwNC0yLjk1Ni0uODEtLjE4OC0yLjExaC0yLjYxbC4yOSAzLjg1NUwxMiAxOS4yODhsNS4zNzMtMS41M0wxOC41OSA0LjQxNHoiLz48L3N2Zz4=",
   },
   {
     id: 2,
-    title: "Junior Software Developer",
-    company: "BackENd",
+    title: "Back ",
+    company: "Back-End",
     yearsActive: "May 2017 - December 2021",
     information: [
-      "Improved and optimized the company's website for a seamless customer experience",
-      "Created, communicated, and managed project plans & project timelines",
-      "Worked to continually meet the expectation of our clients",
+      <SiFirebase />,
+      <FaNode />,
+      <SiMongodb />,
+      <SiExpress />,
+      <SiPassport />,
+      <SiAuth0 />,
+      <FaCloudflare />,
+      <SiMongoose />,
     ],
   },
   {
     id: 3,
-    title: "Junior Software Developer",
+    title: "Tool Stack",
     company: "Tools",
     yearsActive: "February 2016 - April 2017",
     information: [
-      "Planned, developed, tested, deployed, and maintained web applications and mobile applications",
-      "Worked well independently and on a team to solve problems."
+      <VscVscode />,
+      <SiPostman />,
+      <FaChrome />,
+      <SiSublimetext />,
+      <SiWebstorm />,
+      <FiCodesandbox />,
+      <FaGitAlt />,
+      <FaGithub />,
+      <SiNetlify />,
+      <SiVercel />,
+      <SiRender />,
+      <FaWindows />,
+      <SiPrettier />,
+      <BsRegex />,
     ],
   },
 ];
@@ -43,7 +121,7 @@ const Resume = () => {
 
   return (
     <section className="resume container section" id="resume">
-      <h2 className="section__title">Experience</h2>
+      <h2 className="section__title">Tech Stack</h2>
 
       <div className="resume__container">
         <Tabs
@@ -65,25 +143,18 @@ const Resume = () => {
           </TabList>
 
           {WorkExperience.map((experience) => {
-            const { id, company, yearsActive,img, title, information } = experience;
+            const { id, company, yearsActive, title, information } = experience;
             return (
               <TabPanel className="tab__panel" key={`panel-${id}`}>
-                 <div class="css">
-<span class="logo">
-  <img src={img} alt="Logo"/>
-</span>
-<span class="html__text">{yearsActive}</span>
-</div> 
-                {/* <h2 className="tab__panel-title"> */}
-                {/* <img src={img}  className="logo" /> */}
-                 {/* {company} */}
-                {/* </h2> */}
+                <h2 className="tab__panel-title">{title}</h2>
                 {/* <p className="tab__panel-subtitle">{yearsActive}</p> */}
-                {/* <ul className="tab__panel-list">
-                  {information.map((info, index) => {
-                    return <li key={`info-${index}`}>{info}</li>;
-                  })}
-                </ul> */}
+                <ul className="tab__panel-list">
+                  <div className="icon-container">
+                    {information.map((info, index) => {
+                      return <i key={`info-${index}`}>{info}</i>;
+                    })}
+                  </div>
+                </ul>
               </TabPanel>
             );
           })}
@@ -94,6 +165,3 @@ const Resume = () => {
 };
 
 export default Resume;
-
-
-
