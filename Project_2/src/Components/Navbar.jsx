5bfccaa66941d34fc2aa1d534ec975b060ba73fa
karan-raval <karanraval424@gmail.com>
2024-10-17 19:12:17 +0530

    HTTP

--- Project_2/src/Components/Navbar.jsx
+++ Project_2/src/Components/Navbar.jsx
@@ -2,8 +2,6 @@ import {React, useEffect, useState} from "react";
 import "../assets/style.css";
 import { Link } from "react-router-dom";
 const Navbar = () => {
-
-  
   return (
     <>
       <nav className="sidebar">
