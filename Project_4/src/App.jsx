dc56fbf8c078371c30977f701445b2a7bc8810a3
karan-raval <karanraval424@gmail.com>
2024-10-19 21:17:09 +0530

    Blog App

---
+++ Project_4/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Footer from './Components/Footer'
+import Header from './Components/Header'
+import MainRoutes from './Routes/MainRoutes'
+function App() {
+
+  return (
+    <>
+    <Header/>
+    <MainRoutes/>
+    <Footer/>
+    </>
+  )
+}
+
+export default App
---
+++ Project_4/src/Components/Header.jsx
@@ -0,0 +1,94 @@
+import React from 'react'
+import logo from '../assets/images/logo.svg'
+
+const Header = () => {
+  return (
+    <>
+      <div className="s-pageheader">
+
+<header className="header">
+    <div className="header__content row">
+
+        <div className="header__logo">
+            <a className="logo" href="index.html">
+                <img src={logo} alt="Homepage"/>
+            </a>
+        </div>
+
+        <ul className="header__social">
+            <li>
+                <a><i className="fa fa-facebook" aria-hidden="true"></i></a>
+            </li>
+            <li>
+                <a><i className="fa fa-twitter" aria-hidden="true"></i></a>
+            </li>
+            <li>
+                <a><i className="fa fa-instagram" aria-hidden="true"></i></a>
+            </li>
+            <li>
+                <a><i className="fa fa-pinterest" aria-hidden="true"></i></a>
+            </li>
+        </ul> 
+
+        <a className="header__search-trigger" href="#0"></a>
+
+        <div className="header__search">
+
+            <form role="search" method="get" className="header__search-form" action="#">
+                <label>
+                    <span className="hide-content">Search for:</span>
+                    <input type="search" className="search-field" placeholder="Type Keywords" value="" name="s" title="Search for:" autocomplete="off"/>
+                </label>
+                <input type="submit" className="search-submit" value="Search"/>
+            </form>
+
+            <a href="#0" title="Close Search" className="header__overlay-close">Close</a>
+
+        </div>  
+
+        <a className="header__toggle-menu" href="#0" title="Menu"><span>Menu</span></a>
+
+        <nav className="header__nav-wrap">
+
+            <h2 className="header__nav-heading h6">Site Navigation</h2>
+
+            <ul className="header__nav">
+                <li><a href="index.html" title="">Home</a></li>
+                <li className="has-children">
+                    <a href="#0" title="">Categories</a>
+                    <ul className="sub-menu">
+                    <li><a href="category.html">Lifestyle</a></li>
+                    <li><a href="category.html">Health</a></li>
+                    <li><a href="category.html">Family</a></li>
+                    <li><a href="category.html">Management</a></li>
+                    <li><a href="category.html">Travel</a></li>
+                    <li><a href="category.html">Work</a></li>
+                    </ul>
+                </li>
+                <li className="has-children current">
+                    <a href="#0" title="">Blog</a>
+                    <ul className="sub-menu">
+                    <li><a href="single-video.html">Video Post</a></li>
+                    <li><a href="single-audio.html">Audio Post</a></li>
+                    <li><a href="single-gallery.html">Gallery Post</a></li>
+                    <li><a href="single-standard.html">Standard Post</a></li>
+                    </ul>
+                </li>
+                <li><a href="style-guide.html" title="">Styles</a></li>
+                <li><a href="about.html" title="">About</a></li>
+                <li><a href="contact.html" title="">Contact</a></li>
+            </ul> 
+
+            <a href="#0" title="Close Menu" className="header__overlay-close close-mobile-menu">Close</a>
+
+        </nav> 
+
+    </div> 
+</header> 
+
+</div>
+    </>
+  )
+}
+
+export default Header
