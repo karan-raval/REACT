7215500694d902d68e618e912947a5f153e60eac
karan-raval <karanraval424@gmail.com>
2024-10-30 13:42:58 +0530

    Firebase in Blog App Project

---
+++ Project_4/src/Firebase/Firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA9MYbAwnowWTO3ZBb911z1Eg0N8r7-52w",
+  authDomain: "blog-app-d7a65.firebaseapp.com",
+  projectId: "blog-app-d7a65",
+  storageBucket: "blog-app-d7a65.appspot.com",
+  messagingSenderId: "838110091007",
+  appId: "1:838110091007:web:00aa699dfd85d35b251eca"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
