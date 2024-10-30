8dbf520f71311ef33f46de9f63be0394451b1f29
karan-raval <karanraval424@gmail.com>
2024-10-30 12:53:56 +0530

    Login in Blog App Project

--- Project_4/src/Pages/Login.jsx
+++ Project_4/src/Pages/Login.jsx
@@ -1,27 +1,30 @@
-import React from 'react'
-import Header from '../Components/Header'
-import Footer from '../Components/Footer'
+import React from "react";
+import Header from "../Components/Header";
+import Footer from "../Components/Footer";
+import '../assets/css/Login.css'
 const Login = () => {
   return (
     <>
-    <Header/>
-    <div class="login-container">
-  <h1>Login</h1>
-  <form id="loginForm">
-    <div class="input-group">
-      <label for="username">Username</label>
-      <input type="text" id="username" name="username" required>
-    </div>
-    <div class="input-group">
-      <label for="password">Password</label>
-      <input type="password" id="password" name="password" required>
-    </div>
-    <button type="submit">Login</button>
-  </form>
-</div>
-    <Footer/>
+      <Header />
+      <div className="body">
+      <div class="login-container">
+        <h1>Login</h1>
+        <form id="loginForm">
+          <div class="input-group">
+            <label for="username">Username</label>
+            <input type="text" id="username" name="username" required />
+          </div>
+          <div class="input-group">
+            <label for="password">Password</label>
+            <input type="password" id="password" name="password" required />
+          </div>
+          <button type="submit">Login</button>
+        </form>
+      </div>
+      </div>
+      <Footer />
     </>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
