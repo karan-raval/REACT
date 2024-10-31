91ecdf0fde3884e76060676c871c45c6f46c010a
karan-raval <karanraval424@gmail.com>
2024-10-31 12:17:32 +0530

    Login in Blog App Project

--- Project_4/src/Firebase/Firebase.js
+++ Project_4/src/Firebase/Firebase.js
@@ -15,7 +15,6 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
 export const db = getFirestore(app)
 export const auth = getAuth()
 
--- Project_4/src/Pages/Login.jsx
+++ Project_4/src/Pages/Login.jsx
@@ -2,8 +2,63 @@ import React from "react";
 import Header from "../Components/Header";
 import Footer from "../Components/Footer";
 import '../assets/css/Login.css'
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
+import { collection, getDocs } from "firebase/firestore";
+import { useDispatch, useSelector } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
+import { db } from "../FirebaseFolder/Firebase";
+import { toast, ToastContainer } from "react-toastify";
+import { google, userdatas } from "../Redux/User/UserReducer";
+import "react-toastify/dist/ReactToastify.css";
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [username, setUsername] = useState("");
+  const [pass, setPass] = useState("");
+  const [d, setD] = useState([]);
+  const UserCollection = collection(db, "users");
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const state = useSelector((s) => s.UserReducer);
+  console.log(state);
+  console.log(state.isLogin);
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        let data = await getDocs(UserCollection);
+        let val = data.docs.map((el) => ({
+          id: el.id,
+          ...el.data(),
+        }));
+        setD(val);
+      } catch (error) {
+        toast.error("Failed to load user data", { autoClose: 3000 });
+      }
+    };
+    getData();
+  }, []);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    let user = d.filter((el) => el.email === email && el.pass === pass);
+    console.log(user);
+    if (user.length > 0) {
+      toast.success("Login Successful!", { autoClose: 3000 });
+      dispatch(userdatas)(user[0]);
+      setTimeout(() => {
+        navigate("/");
+      }, 3000);
+    } else {
+      toast.error("Login Unsuccessful! Please check your credentials.", {
+        autoClose: 3000,
+      });
+    }
+  };
+
+  const handleClick = () => {
+    dispatch(google);
+  };
   return (
     <>
       <Header />
@@ -11,13 +66,20 @@ const Login = () => {
       <div class="login-container">
         <h1>Login</h1>
         <form id="loginForm">
-          <div class="input-group">
+
+        <div class="input-group">
             <label for="username">Username</label>
-            <input type="text" id="username" name="username" required />
+            <input type="text" onChange={(e) => setUsername(e.target.value)}  name="username" required />
           </div>
+
+          <div class="input-group">
+            <label for="email">Email</label>
+            <input type="text" onChange={(e) => setEmail(e.target.value)}  name="email" required />
+          </div>
+
           <div class="input-group">
             <label for="password">Password</label>
-            <input type="password" id="password" name="password" required />
+            <input type="password" onChange={(e) => setPass(e.target.value)}  name="password" required />
           </div>
           <button type="submit">Login</button>
         </form>
