8f41340dc3cb559cce64054d31f777613aef0b9f
karan-raval <karanraval424@gmail.com>
2025-12-24 00:06:41 +0530

    UrbanRoute Travels

--- UrbanRoute Travels/src/App.jsx
+++ UrbanRoute Travels/src/App.jsx
@@ -1,16 +1,11 @@
 import './App.css'
-import './assets/css/style.scss'
-import HeroSection from './Components/HeroSection'
 import Navbar from './Components/Navbar'
-import SectionOne from './Components/SectionOne'
 
 function App() {
 
   return (
     <>
     <Navbar/>
-    <HeroSection/>
-    {/* <SectionOne/> */}
     </>
   )
 }
--- UrbanRoute Travels/src/Components/Navbar.jsx
+++ UrbanRoute Travels/src/Components/Navbar.jsx
@@ -1,49 +1,45 @@
-import React from 'react'
+import React, { useState } from "react";
+import logo from "../assets/image/Logo.png"; 
 
 const Navbar = () => {
-  return (
-    <>
-    <header className="header_section">
-      <div className="container-fluid">
-        <nav className="navbar navbar-expand-lg custom_nav-container ">
-          <a className="navbar-brand" href="index.html">
-            <span>
-              Urotaxi
-            </span>
-          </a>
-          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
+  const [menuOpen, setMenuOpen] = useState(false);
 
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <div className="d-flex ml-auto flex-column flex-lg-row align-items-center">
-              <ul className="navbar-nav  ">
-                <li className="nav-item active">
-                  <a className="nav-link" href="index.html">Home <span className="sr-only">(current)</span></a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="about.html"> About</a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="service.html"> Services </a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="news.html"> News</a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="contact.html">Contact Us</a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="#">Login</a>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </nav>
+  return (
+    <header className="navbar">
+      <div className="nav-container">
+        
+        {/* Logo */}
+        <div className="nav-logo">
+          <img src={logo} alt="UrbanRoute Travels" />
+          <span>UrbanRoute Travels</span>
+        </div>
+
+        {/* Menu */}
+        <ul className={`nav-links ${menuOpen ? "active" : ""}`}>
+          <li><a href="#home">Home</a></li>
+          <li><a href="#about">About</a></li>
+          <li><a href="#services">Services</a></li>
+          <li><a href="#collection">Collection</a></li>
+          <li><a href="#contact">Contact</a></li>
+          <li className="book-btn">
+            <a href="https://wa.me/91XXXXXXXXXX" target="_blank" rel="noreferrer">
+              Book Now
+            </a>
+          </li>
+        </ul>
+
+        {/* Mobile Menu Icon */}
+        <div
+          className="menu-toggle"
+          onClick={() => setMenuOpen(!menuOpen)}
+        >
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
       </div>
     </header>
-    </>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
